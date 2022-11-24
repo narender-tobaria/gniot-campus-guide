99a339cee0c91070e26d6d172aca97ce9344350e
narender-tobaria <narendertobaria@gmail.com>
2022-11-24 15:37:33 +0530

    index page responsive completed

--- footer.php
+++ footer.php
@@ -26,33 +26,3 @@
     </div>
     <div class="copyright-content">2022 © GNIOT Campus Guide. All rights reserved.</div>
 </footer>
-
-
-<!-- <footer class="site-footer">
-    <div class="footer-content">
-        <div class="first-column">
-            <div class="footer-logo"><img class="footer-logo" src="assets/logo-gniot.jpg" alt=""></div>
-            <div class="below-logo-content">Discover amazing things in GNIOT</div>
-        </div>
-        <div class="second-column">
-            <div class="footer-pages">
-                <a href="">About Us</a>
-                <a href="">Feedback</a>
-            </div>
-        </div>
-        <div class="third-column">
-            <h3>Contact Us</h3>
-            <div class="footer-contact-details">
-                <p>Email:  ngyatra@gmail.com</p>
-                <p>Phone: 1 (00) 832 2342</p>
-                <div class="social-icons">
-                    <i class="bi bi-facebook"></i>
-                    <i class="bi bi-instagram"></i>
-                    <i class="bi bi-twitter"></i>
-                    <i class="bi bi-youtube"></i>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="copyright-content">2022 © GNIOT Campus Guide. All rights reserved.</div>
-</footer> -->
